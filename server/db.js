c9073d5d80fa2e8384e0f69a46e629a7247a229e
HidayahJadaan <hkaljadaan18@cit.just.edu.jo>
2024-02-02 19:34:21 +0300

    Register Is Done

--- server/db.js
+++ server/db.js
@@ -1,18 +1,9 @@
-import mysql from 'mysql';
-
+import mysql from "mysql"
 
 export const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"root",
-    port: 3306,
-    database:"mysqlBlogg",
+  host:"localhost",
+  user:"root",
+  password:"rootPassword&",
+  port: 3306,
+  database:"mysqlBlogg",
 })
-
-// db.connect(err => {
-//     if (!err) {
-//       console.log("DB Connection Succeeded");
-//     } else {
-//       console.log("DB Connection Failed", err);
-//     }
-// });
